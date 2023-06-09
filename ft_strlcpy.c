e95b3ac2e91130b56cf857f266616585f97e7e53
Nao Sakanoue <nsakanou@c1r23s5.42tokyo.jp>
2023-06-09 14:51:32 +0900

    memchr&strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 12:18:19 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/06/08 14:50:49 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/06/09 14:32:11 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	if (dst == NULL || src == NULL)
-		return (0);
 	if (dstsize == 0)
 		return (ft_strlen(src));
 	while (src[i] != '\0' && i < (dstsize - 1))
